d01f6fd55aa3596dedf08973949e65507211327a
juancerron88 <juancerron88@gmail.com>
2023-04-24 21:31:29 -0500

    actualización 24/04/2023

---
+++ semana8/dia5/react-router/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Router from './router/router'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <div>       
+      <Router/>      
+    </div>
+  )
+}
+
+export default App
